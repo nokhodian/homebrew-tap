a7bcde2164baaa9506f5bfac24e076639af966af
nokhodian <nokhodian@gmail.com>
2026-03-20 14:33:13 +0100

    Add Monotask desktop app Cask v0.2.0

---
+++ Casks/monotask.rb
@@ -0,0 +1,22 @@
+cask "monotask" do
+  version "0.2.0"
+  sha256 "931ca0059db1170c5e814824c4133f1c1fc196bdc54fc7482a92b1fa7158c644"
+
+  url "https://github.com/nokhodian/monotask/releases/download/v#{version}/Monotask-v#{version}-aarch64.dmg"
+  name "Monotask"
+  desc "Local-first peer-to-peer kanban board with cryptographic identity"
+  homepage "https://github.com/nokhodian/monotask"
+
+  depends_on macos: ">= :ventura"
+
+  disable_quarantine true
+
+  app "Monotask.app"
+
+  zap trash: [
+    "~/.local/share/p2p-kanban",
+    "~/Library/Application Support/dev.monotask",
+    "~/Library/Preferences/dev.monotask.plist",
+    "~/Library/Saved Application State/dev.monotask.savedState",
+  ]
+end
